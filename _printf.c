52735236b91bc33fd15ac8341ad2a92f34e96c33
gavintobin <gavintobin714@gmail.com>
2022-10-28 10:50:19 -0700

    need to add i &c case

--- _printf.c
+++ _printf.c
@@ -1,6 +1,9 @@
 #include "main.h"
 #include <stdio.h>
-#include <stddarg>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <string.h>
 
 /**
  * _printf - printf function
@@ -11,51 +14,95 @@
 
 int _printf(const char *format, ...)
 {
-	char *traverse;
-	unsigned int i;
-	char *s;
 
-	va_list arg;
-	va_start(arg, format);
+	va_list valist;
+	int num = 0;
+	char *token = NULL;
+	int i = 0;
+	int len = strlen(format);
+	int nprinted = 0;
+	int found = 0;
+	int str[40];
+	int temp = num;
+	 int j = 0;
 
-	for (traverse = format; *traverse != '\0' ; traverse++)
+	va_start(valist, format);
+
+	if (format == NULL)
+		return 0;
+
+	while (format[i] != ' ')
 	{
-		while (*traverse != '%')
+		num = 0;
+		found = 0;
+		token = NULL;
+
+		if ((format[i] == '%') && ((i + 1) < len))
 		{
-			putchar(*traverse);
-			traverse++;
+			switch (format[i+1])
+			{
+				case 'd' :
+					{
+						found = 1;
+
+						num = va_arg(valist, int);
+
+						if (num < 0)
+							num = -num;
+
+						while (num != 0)
+						{
+							str[j++] = (num % 10);
+							num /= 10;
+						}
+						if (temp < 0)
+							str[j++] = '-';
+
+						nprinted += j;
+						j--;
+
+						while (j >= 0)
+						{
+							if (str[j] != '-')
+								putchar(str[j--] + '0');
+							else
+								putchar(str[j--]);
+						}
+					}
+					break;
+
+				case 's' :
+					{
+						found = 1;
+
+						token = va_arg(valist, char *);
+						if (token != NULL)
+						{
+
+							while (token[j] != ' ')
+							{
+								nprinted++;
+								putchar(token[j]);
+								j++;
+							}
+						}
+					}
+					break;
 		}
-		if (*traverse == '\0')
-			return;
-		switch (*traverse)
+
+		if (found != 0)
 		{
-			case 'c' : i = va_arg(arg, int);
-				   if (i < 0)
-				   {
-					   i = -i;
-					   pputchar('-');
-				   }
-				   puts(convert(i,10));
-				   break;
-			case 's' : s = va_arg(arg, char *);
-				   puts(s);
-				   break;
-			case 'd' : d =va_arg(arg, int);
-				   if (i < 0)
-				   {
-					   i = -i;
-					   putchar('-');
-				   }
-				   puts(convert(i,10));
-				   break;
-			case 'o' : i = va_arg(arg, unsigned int);
-				   puts(convert(i,8));
-				   break;
-			case 'x' : i = va_arg(arg, unsigned int);
-				   puts(convert(i,16));
-				   break;
+			i += 2;
+			continue;
 		}
 	}
-	va_end(arg);
+		putchar(format[i]);
+		nprinted++;
+
+		i++;
+	}
+	
+	va_end(valist);
+	return nprinted;
 }
 
